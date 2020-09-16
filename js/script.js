0ff563b32f95f5a9d20378a1b6995c36b41f9bef
AmaryllisNO <amaryllisno@gmail.com>
2020-09-16 21:34:49 +0200

    lesson 2 task complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const url = "https://noroffcors.herokuapp.com/https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/breakingbad";
-
-const resultContainer = document.querySelector(".result-container");
+import { url, resultContainer } from "./constants/constants.js";
+import { displayMessage} from "./components/displayMessage.js";
+import createTags from "./components/createTags.js";
 
 async function callApi() {
     try {
@@ -15,10 +15,6 @@ async function callApi() {
 }
 
 callApi();
-
-function displayMessage(type, message) {
-    return `<div class="message ${type}">${message}</div>`;
-}
 
 function createHtml(data) {
     resultContainer.innerHTML = "";
@@ -35,10 +31,3 @@ function createHtml(data) {
     }
 }
 
-function createTags(list = []) {
-    let tags = "";
-
-    list.forEach((item) => (tags += `<span>${item}</span>`));
-
-    return tags;
-}
